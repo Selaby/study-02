59ae33f613347415b590432f3ba3ad2b297f267a
Selaby <80803764+Selaby@users.noreply.github.com>
2021-04-20 22:01:41 +0900

    Add files via upload
    2-7

--- 2.py
+++ 2.py
@@ -2,8 +2,11 @@ import os
 from selenium.webdriver import Chrome, ChromeOptions
 import time
 import pandas as pd
+import datetime
 
-# Chromeを起動する関数
+LOG_FILE_PATH = "mynavi.log"
+
+### Chromeを起動する関数
 def set_driver(driver_path, headless_flg):
     # Chromeドライバーの読み込み
     options = ChromeOptions()
@@ -23,6 +26,15 @@ def set_driver(driver_path, headless_flg):
     # ChromeのWebDriverオブジェクトを作成する。
     return Chrome(executable_path=os.getcwd() + "/" + driver_path, options=options)
 
+### ログファイルおよびコンソール出力
+def log(txt):
+    now=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    logStr = '[%s: %s] %s' % ('log',now , txt)
+    # ログ出力
+    with open(LOG_FILE_PATH, 'a', encoding='utf-8_sig') as f:
+        f.write(logStr + '\n')
+    print(logStr)
+
 def find_table_target_word(th_elms, td_elms, target:str):
     # tableのthからtargetの文字列を探し一致する行のtdを返す
     for th_elm,td_elm in zip(th_elms,td_elms):
@@ -31,7 +43,9 @@ def find_table_target_word(th_elms, td_elms, target:str):
 
 # main処理
 def main():
+    log("処理開始")
     search_keyword = input("検索キーワードを入力　＞＞　")
+    log("検索キーワード:{}".format(search_keyword))
     # driverを起動
     if os.name == 'nt': #Windows
         driver = set_driver("chromedriver.exe", False)
@@ -61,6 +75,11 @@ def main():
     exp_naiyou_list = []
     exp_kyuyo_list = []
     exp_nenshu_list = []
+    
+    # ログ出力用の変数
+    count = 1
+    success = 0
+    fail = 0
 
     # 3項目を50件目まで取得
     while True:
@@ -69,13 +88,24 @@ def main():
 
         # 1ページ分繰り返し
         for table in table_list:
-            # 3項目をtableから探す
-            naiyou = find_table_target_word(table.find_elements_by_tag_name("th"), table.find_elements_by_tag_name("td"), "仕事内容")
-            exp_naiyou_list.append(naiyou)
-            kyuyo = find_table_target_word(table.find_elements_by_tag_name("th"), table.find_elements_by_tag_name("td"), "給与")
-            exp_kyuyo_list.append(kyuyo)
-            nenshu = find_table_target_word(table.find_elements_by_tag_name("th"), table.find_elements_by_tag_name("td"), "初年度年収")
-            exp_nenshu_list.append(nenshu)
+            try:
+            # try~exceptはエラーの可能性が高い箇所に配置                
+                # 3項目をtableから探す
+                naiyou = find_table_target_word(table.find_elements_by_tag_name("th"), table.find_elements_by_tag_name("td"), "仕事内容")
+                exp_naiyou_list.append(naiyou)
+                kyuyo = find_table_target_word(table.find_elements_by_tag_name("th"), table.find_elements_by_tag_name("td"), "給与")
+                exp_kyuyo_list.append(kyuyo)
+                nenshu = find_table_target_word(table.find_elements_by_tag_name("th"), table.find_elements_by_tag_name("td"), "初年度年収")
+                exp_nenshu_list.append(nenshu)
+                log(f"{count}件目成功 : {naiyou}")
+                success+=1
+            except Exception as e:
+                log(f"{count}件目失敗 : {naiyou}")
+                log(e)
+                fail+=1
+            finally:
+                # finallyは成功でもエラーでも必ず実行
+                count+=1
 
         # ページ送り    
         next_page = driver.find_elements_by_class_name("iconFont--arrowLeft")
@@ -83,13 +113,14 @@ def main():
             next_page_link = next_page[0].get_attribute("href")
             driver.get(next_page_link)
         else:
-            print("最終ページです。終了します。")
+            log("最終ページです。終了します。")
             break
     
     df = pd.DataFrame({"仕事内容":exp_naiyou_list,
                        "給与":exp_kyuyo_list,
                        "初年度年収":exp_nenshu_list})
-    df.to_csv("mynavi.csv", encoding="utf_8-sig")
+    df.to_csv("mynavi.csv", encoding="utf-8-sig")
+    log(f"処理完了 成功件数: {success} 件 / 失敗件数: {fail} 件")
     print("csvファイルを出力しました")
     
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
